48b6cece9169affa36c2f8eb7c7341d1217c7db2
Sungsu Kim <sungsu@g.yju.ac.kr>
2026-04-05 14:42:03 +0900

    add utils and memo

---
+++ utils/git_tools.py
@@ -0,0 +1,31 @@
+# 유용한 깃 명령어 정리
+
+"""
+git status
+git add .
+git add file.txt
+git commit -m "메세지"
+
+git commit -am "메세지"
+
+git diff
+git diff --staged
+git log
+git log --oneline
+
+git branch -d name
+git branch -D name
+
+git clone URL
+git pull
+git push
+git push -u origin branch-name
+
+git reset file.txt
+git checkout -- file.txt
+git commit --amend
+
+git stash
+git stash pop
+git stash list
+"""
